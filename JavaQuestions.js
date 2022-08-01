58c51d7e14eaa580f1f2e888dafa2a22bf39b1d3
Jyothi-Chellappan-Ampally <71203041+Jyothi-Chellappan-Ampally@users.noreply.github.com>
2022-08-01 17:07:58 +0530

    Create JavaQuestions.js

---
+++ JavaQuestions.js
@@ -0,0 +1,125 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+  {
+  numb: 1,
+  question: " Which of these components are used in a Java program for compilation, debugging, and execution?",
+  answer: "JDK",
+  options: [
+    "JDK",
+    "JRE",
+    "ClassLoader",
+    "Debugger"
+  ]
+},
+  {
+  numb: 2,
+  question: "Which of these literals can be contained in a float data type variable?",
+  answer: "-3.4e+038",
+  options: [
+    "-3.4e+038",
+    "+1.7e+308",
+    "-3.4e+038",
+    "-1.7e+308"
+  ]
+},
+  {
+  numb: 3,
+  question: " What is BigDecimal.ONE?",
+  answer: "it is a static variable that has a value of 1 on a scale of 0",
+  options: [
+    "it is a custom-defined statement",
+    "it is a wrong statement",
+    "it is a static variable that has a value of 1 on a scale of 0",
+    "it is a static variable that has a value of 1 on a scale of 10"
+  ]
+},
+  {
+  numb: 4,
+  question: " When an expression consists of int, double, long, float, then the entire expression will get promoted into a data type that is:",
+  answer: "double",
+  options: [
+    "float",
+   "double",
+  "int",
+    "long"
+  ]
+},
+  {
+  numb: 5,
+  question: "Which of the following operators can operate on a boolean variable?",
+  answer: "A,B and C",
+  options: [
+    "&&","==","?:","+="
+  ]
+},
+    {
+    numb: 6,
+    question: " Out of these statements, which ones are incorrect?",
+    answer: "The division operator / has comparatively higher precedence as compared to a multiplication operator",
+    options: [
+     "The Brackets () have the highest precedence","The equal to = operator has the lowest precedence",
+      "The addition operator + and the subtraction operator – have an equal precedence","The division operator / has comparatively higher precedence as compared to a multiplication operator"
+    ]
+  },
+   {
+    numb: 7,
+    question: " What will be the output of the following Java program?class Output
+
+{
+
+public static void main(String args[])
+
+{
+
+int p = 1;
+
+int q = 2;
+
+int r = 3;
+
+p |= 4;
+
+q >>= 1;
+
+r <<= 1;
+
+p ^= r;
+
+System.out.println(q + ” ” + q + ” ” + r);
+
+}
+
+}",
+    answer: "3 1 6",
+    options: [
+    "3 3 6",
+    "2 3 4",
+    "2 2 3",
+    "3 1 6"
+    ]
+  },
+   {
+    numb: 8,
+    question: "What is it called when the child object also gets killed when the parent object is killed in the program?",
+    answer: "Composition",
+    options: [
+    "Encapsulation","Association","Aggregation","Composition"
+    ]
+  },
+   {
+    numb: 9,
+    question: " How does one identify if a compilation unit is an interface or class from a .class file?",
+    answer: "Java source file header",
+    options: [
+    "Extension of the compilation unit","Java source file header","The class and interface cannot be differentiated","The unit type must be used to postfix interface or class name"
+    ]
+  },
+   {
+    numb: 10,
+    question: " Out of these methods of the String class, which one can be used for testing the strings for equality?",
+    answer: "equals()",
+    options: [
+     "isequals()","isequal()","equals()","equal()"
+    ]
+  },
+];
